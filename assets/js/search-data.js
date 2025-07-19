5e909dfb634fe3c5372894fead105b2b4c95e752
YichengDuan <yxd245@case.edu>
2025-07-19 03:44:45 +0000

    Deploying to gh-pages from @ YichengDuan/YichengDuan.github.io@9634c7fcb3983ae9ab62da97c2dee2a60b15d03a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -47,51 +47,26 @@ ninja.data = [{
         },{id: "news-everything-starts-here-stay-tuned-for-the-latest-updates-and-announcements",
           title: 'Everything starts here. Stay tuned for the latest updates and announcements. 🦾',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-an-agentic-navigation-framework-utilizing-vision-language-models",
+          title: 'An Agentic Navigation Framework Utilizing Vision-Language Models',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-enhancing-video-retravel-using-vlm",
+          title: 'Enhancing Video Retravel Using VLM',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-low-rank-adaptation-defense-with-robustness",
+          title: 'Low-Rank Adaptation Defense with Robustness',
+          description: "A LoRA-based defense pipeline for a ResNet-18 model",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project.html";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-topological-graph",
+          title: 'Topological Graph',
+          description: "A Topological Graph Framework for Enhanced Scene Detection and Understanding in Robotic Navigation",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project.html";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project.html";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project.html";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project.html";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project.html";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project.html";
             },},{
         id: 'social-email',
         title: 'email',
